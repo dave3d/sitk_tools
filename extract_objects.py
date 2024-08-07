3c55ac0227ff7118f4db5856fc0143570b0cd9b7
Dave Chen <dchen@mail.nih.gov>
2024-08-07 17:25:02 -0400

    Re-factor for pylint

--- extract_objects.py
+++ extract_objects.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python
 
-import sys
+""" Script to extract the N largest objects from a binary mask image. """
 import argparse
 import SimpleITK as sitk
+
+def extract_object(obj_id, vol, kernel_radius, output_name):
+    """Extract the object with the given ID from the volume and write it to a file."""
+    obj_vol = vol == obj_id
+
+    # clean up the volume
+    obj_vol = sitk.BinaryFillhole(obj_vol)
+    obj_vol = sitk.BinaryClosingByReconstruction(
+        obj_vol, [kernel_radius, kernel_radius, kernel_radius]
+    )
+
+    print("Writing", output_name)
+    sitk.WriteImage(obj_vol, output_name)
 
 
 def extract_objects(
@@ -17,49 +30,35 @@ def extract_objects(
     shape.Execute(objs)
     counts = {}
 
-    xdim = input_mask.GetWidth()
-    ydim = input_mask.GetHeight()
+    dim2 = [input_mask.GetWidth(), input_mask.GetHeight()]
 
     for i in range(1, shape.GetNumberOfLabels() + 1):
         counts[i] = shape.GetNumberOfPixels(i)
 
     # Sort the objects by number of pixels in each object
-    sorted_counts = {
-        k: v for k, v in sorted(counts.items(), key=lambda item: item[1], reverse=True)
-    }
+    sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
 
     # Extract the n largest objects
     ids = list(sorted_counts)
     for i in range(n):
 
         obj_id = ids[i]
-        count = sorted_counts[obj_id]
-        print(obj_id, count)
+        print(obj_id, sorted_counts[obj_id])
 
+        # Check the compactness of the object
         bounds = shape.GetBoundingBox(obj_id)
         print("\nBounds:", bounds)
-        xc = bounds[3] / xdim
-        yc = bounds[4] / ydim
-        print("Compactness:", xc, yc)
-        if xc > compactness or yc > compactness:
+        comp = [bounds[3] / dim2[0], bounds[4] / dim2[1]]
+        print("Compactness:", comp)
+        if comp[0] > compactness or comp[1] > compactness:
             print("Skipping object")
             continue
 
-        # select out the object's voxels
-        obj_vol = objs == obj_id
-
-        # clean up the volume
-        obj_vol = sitk.BinaryFillhole(obj_vol)
-        obj_vol = sitk.BinaryClosingByReconstruction(
-            obj_vol, [kernel_radius, kernel_radius, kernel_radius]
-        )
-
-        obj_name = f"{output_name}_{i:02d}.nii.gz"
-        print("Writing", obj_name)
-        sitk.WriteImage(obj_vol, obj_name)
+        extract_object(obj_id, objs, kernel_radius, f"{output_name}_{i:02d}.nii.gz")
 
 
 def parse_args():
+    """ Parse command line arguments. """
     parser = argparse.ArgumentParser()
 
     parser.add_argument("filenames", nargs="*")
@@ -103,8 +102,7 @@ def parse_args():
         help="radius for binary fill hole filter",
     )
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -114,11 +112,11 @@ if __name__ == "__main__":
     mask_name = args.filenames[0]
 
     if len(args.output) > 0:
-        output_name = args.output
+        out_name = args.output
     else:
         words = mask_name.split(".")
-        output_name = words[0] + ".obj"
+        out_name = words[0] + ".obj"
 
     mask = sitk.ReadImage(mask_name)
 
-    extract_objects(mask, output_name, args.n, args.compact, args.radius)
+    extract_objects(mask, out_name, args.n, args.compact, args.radius)
